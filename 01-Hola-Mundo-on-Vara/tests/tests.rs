74d5a9a9520cc57419380d6dff0a031c758b9a69
A Mon <a.mont.24.777@gmail.com>
2024-01-23 07:44:04 -0600

    Update

---
+++ 01-Hola-Mundo-on-Vara/tests/tests.rs
@@ -0,0 +1,17 @@
+use gtest::{Log, Program, System};
+
+#[test]
+fn hello_test() {
+    let sys = System::new();
+    sys.init_logger();
+    let program = Program::current(&sys);
+
+    let res = program.send(2, String::from("Hello"));
+    
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(String::from("Hello"));
+    assert!(res.contains(&expected_log));
+
+
+}
